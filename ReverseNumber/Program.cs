dfdaeb11c4fa2bfbf91ae3fcb2cae0653b0521c8
vaishnavipachpor <59760241+vaishnavipachpor@users.noreply.github.com>
2023-09-06 16:28:59 +0530

    Add project files.

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿// Write the program to print the reverse of the given Number.
+ class ReverseNumber
+{
+static void Main()
+    {
+        Console.Write("Enter the number");
+        int Number = int.Parse(Console.ReadLine());
+        int Reminder, Reverse = 0;
+        while(Number!=0)
+        {
+            Reminder = Number % 10;
+            Reverse = Reverse * 10 + Reminder;
+            Number = Number / 10;
+
+        }
+        Console.WriteLine("reverse number is" + Reverse);
+        Console.ReadLine();
+
+    }
+
+}
